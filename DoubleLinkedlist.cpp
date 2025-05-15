987a3da8ac5627f675315eef2e1fcce06ed84173
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-15 11:04:55 +0700

    Step 3

--- DoubleLinkedlist.cpp
+++ DoubleLinkedlist.cpp
@@ -171,8 +171,14 @@ class Node
             // Step 2: Traverse backward
             cout << "\nRecords in descending order of roll number are:\n";
             while (currentNode != NULL)
-            
-
+            {
+                cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+    
+                // Step 3: Move to previous node
+                currentNode = currentNode->prev;
+                i--;
+            }
+        
          
     
     
