100382714fdcb8a234d011a2edc63eeb52532dc3
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-15 10:04:50 +0700

    Set prev dari START ke newNode jika START ada

--- DoubleLinkedlist.cpp
+++ DoubleLinkedlist.cpp
@@ -41,7 +41,14 @@ class Node
                     {
                         cout << "\nDuplicate number not allowed" << endl;
                         return;
-                   
+                    }
+                    // Step 4: newNode.next = START
+                    newNode->next = START;
+
+                    // Step 5: START.prev = newNode (if START exists)
+                    if (START != NULL)
+                        START->prev = newNode;
+                }
                 
                    
             
