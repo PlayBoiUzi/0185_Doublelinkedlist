0651bbe77d6a996823323a131c169dd31f649537
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-15 10:23:30 +0700

    Hubungkan newNode ke Curretnt->next

--- DoubleLinkedlist.cpp
+++ DoubleLinkedlist.cpp
@@ -74,8 +74,6 @@ class Node
 
                 // Step 9: Insert between current and current->next
                 newNode->next = current->next; // Step 9a: newNode.next = current.next
-                newNode->prev = current;      // Step 9b: newNode.prev = current
-
                 
                 
                    
